a9d42d32351ebe4473733305a3b9f2ad8c2a4180
Kelly A Porter <78833247+KellyPorter02@users.noreply.github.com>
2021-05-03 21:39:14 -0400

    fixed start.js file

--- src/start.js
+++ src/start.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-app.use(express.static(__dirname + '/dist/army-time-converter')); // myApp will be the same folder name.
+app.use(express.static(__dirname + '/dist/LCS-inventory-frontend')); // myApp will be the same folder name.
 app.get('/*', function (req, res) {
-  res.sendFile(path.join(__dirname + '/dist/army-time-converter/index.html'));
+  res.sendFile(path.join(__dirname + '/dist/LCS-inventory-frontend/index.html'));
 });
 app.listen(process.env.PORT || 3000);
